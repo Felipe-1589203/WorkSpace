a2c647290b4b2ae62328c0e40ab166feb51deb44
Felipe Souza <felipe_135791@hotmail.com>
2023-08-26 10:44:34 -0300

    4

--- Projeto-Primo/API.py
+++ Projeto-Primo/API.py
@@ -4,5 +4,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+class hoteis(Resource):
+    def get(self):
+        return {'hoteis': 'Meus hoteis'}
 
+api.add_resource(hoteis, '/hoteis')
+if __name__  == '__main__':
+    app.run(debug=True)    
 
