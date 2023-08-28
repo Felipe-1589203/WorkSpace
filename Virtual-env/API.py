b7c9e40c203b10aebab0fe61787d0b3fe7f5cbb4
Felipe Souza <felipe_135791@hotmail.com>
2023-08-28 09:38:58 -0300

    1.

--- Virtual-env/API.py
+++ Virtual-env/API.py
@@ -1,38 +1,11 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
+from Resources.hotel import Hoteis
 
 app = Flask(__name__)
 api = Api(app)
 
-hoteis = [
-    {
-        'hotel_id':'alpha',
-        'nome': 'Alpha Hotel',
-        'estrelas': 4.5,
-        'diaria': 420.34,
-        'cidade': 'Osasco'
-    },
-    {
-        'hotel_id':'bravo',
-        'nome': 'Bravo Hotel',
-        'estrelas': 4.4,
-        'diaria': 380.90,
-        'cidade': 'Sorocaba'
-    },
-    {
-        'hotel_id':'Felipe',
-        'nome': 'Felipe Hotel',
-        'estrelas': 4.7,
-        'diaria': 500.53,
-        'cidade': 'Itapevi'
-    }
-]
-
-class Hoteis(Resource):
-    def get(self):
-        return {'hoteis': hoteis}
-
-api.add_resource(Hoteis, '/hoteis')
+api.add_resources(Hoteis, '/hoteis')
 if __name__  == '__main__':
     app.run(debug=True)    
 
